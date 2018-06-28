5f8a4b3ca302bc67360f73a753a1086235f315be
Laura Tebben <Laura.Tebben@ge.com>
2018-06-28 15:06:08 -0400

    Super simple tests

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestCases(unittest.TestCase):
+    def test_1(self): 
+        self.assertTrue(True)
+    def test_2(self):
+        self.assertTrue(False)
+
+if __name__ == "__main__":
+    unittest.main()
